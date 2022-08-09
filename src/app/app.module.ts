1375ceb3dfbf93d7deeb99d16e3c223097d71c67
Adam Hauze <adam.hauze@energycap.com>
2022-08-09 13:13:29 -0600

    Generated cookie and cookie-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CookieComponent } from './cookie/cookie.component';
+import { CookieListComponent } from './cookie-list/cookie-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CookieComponent,
+    CookieListComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cookie-list/cookie-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cookie-list',
+  templateUrl: './cookie-list.component.html',
+  styleUrls: ['./cookie-list.component.css']
+})
+export class CookieListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
